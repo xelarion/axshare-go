c3b4cc8e5f83dea111ad77fc49705725db552b32
ervincheung <1390838101@qq.com>
2019-08-31 18:02:14 +0800

    fix(response format): use new response format

--- api/v1/axure/axures.go
+++ api/v1/axure/axures.go
@@ -4,6 +4,7 @@ import (
 	"axshare_go/internal/db"
 	"axshare_go/internal/models"
 	"github.com/gin-gonic/gin"
+	go_rsp "github.com/standard-rsp/go-rsp"
 	"net/http"
 	"strconv"
 )
@@ -17,5 +18,8 @@ func GetAxures(c *gin.Context) {
 	axureGroupId, _ := strconv.ParseUint(c.Param("axure_group_id"), 10, 64)
 	var axures []models.Axure
 	db.AxshareDb.Where(&models.Axure{AxureGroupID: uint(axureGroupId)}).Find(&axures)
-	c.JSON(http.StatusOK, FormatList(axures))
+	c.JSON(http.StatusOK, go_rsp.RspPagData(
+		FormatList(axures), go_rsp.OK,
+		go_rsp.NewBaseMessage("", ""),
+		go_rsp.BasePaginate{CurrentPage: 1, TotalPages: 20, TotalCount: 201, PerPage: 10}))
 }
