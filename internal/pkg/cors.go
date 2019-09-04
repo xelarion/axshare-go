7750ea0a69f05c29e487e5c7f1019d274247dd8e
ervincheung <1390838101@qq.com>
2019-09-04 17:11:38 +0800

    fix(cors): add 'Authorization' to AllowHeaders

--- internal/pkg/cors.go
+++ internal/pkg/cors.go
@@ -5,11 +5,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"os"
 	"strings"
+	"time"
 )
 
 // 解决跨域问题
 func AllowRouterCors(router *gin.Engine) {
-	config := cors.DefaultConfig()
+	config := cors.Config{
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
 	config.AllowOrigins = strings.Split(os.Getenv("allow_origins"), ",")
 	router.Use(cors.New(config))
 }
