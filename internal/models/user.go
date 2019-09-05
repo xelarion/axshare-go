18f9912c02415b0e4a199b4ba50005d142de327a
ervincheung <1390838101@qq.com>
2019-09-05 10:59:58 +0800

    fix(models/user.go): change User struct json columns

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,9 +5,9 @@ import (
 )
 
 type User struct {
-	ID        uint `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Email     string
-	Nickname  string
+	ID        uint      `json:"created_at" gorm:"primary_key"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+	Nickname  string    `json:"nickname"`
 }
