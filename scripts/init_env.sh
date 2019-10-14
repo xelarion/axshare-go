0b7b9b9031bb989f2c68f5c49dd2e2b51d13baa6
ervincheung <1390838101@qq.com>
2019-10-14 19:44:25 +0800

    fix(scripts/init_env.sh): install docker-compose

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -24,5 +24,8 @@ sudo apt-get install docker-ce=5:19.03.2~3-0~ubuntu-bionic docker-ce-cli=5:19.03
 sudo docker run hello-world
 
 # install unar
-sudo apt-get install unar -y
+#sudo apt-get install unar -y
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
